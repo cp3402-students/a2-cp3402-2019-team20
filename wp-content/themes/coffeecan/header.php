24b136fab7c9fd5f5141c87db200bd5e9077b0e2
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-05-10 19:11:25 +1000

    created a sticky nav bar with a color changing bar. after 2000 pixels the menu breaks and continues to the right. need to fix

--- wp-content/themes/coffeecan/header.php
+++ wp-content/themes/coffeecan/header.php
@@ -17,13 +17,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
+
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
 <div id="page" class="site">
     <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'coffeecan'); ?></a>
-
     <?php if (get_header_image()) : ?> <!-- If statement to check if header image is set-->
         <figure class="header-image-container transparent">
             <?php the_header_image_tag(); ?>
@@ -114,3 +115,4 @@
 
 
     <div id="content" class="site-content">
+        <script src="/wp-content/themes/coffeecan/js/scroll.js"></script>
