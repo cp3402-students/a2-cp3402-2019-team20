ca41cb3070909354be1b57e54885d690c9b53aeb
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-05-10 10:58:24 +1000

    fixed php code in header.php to chekconly if header image exists to generate site-header class block, added and else clause in header.php to generate site-header-basic class if no featured image is present, add site-header-basic to css properties to inherit and overwite site-basic css rules, added box shadow to body site-content class

--- wp-content/themes/coffeecan/header.php
+++ wp-content/themes/coffeecan/header.php
@@ -24,7 +24,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'coffeecan' ); ?></a>
 
-    <?php if ( get_header_image() and is_front_page()) : ?> <!-- If statement to check if header image is set and user is on home page -->
+    <?php if ( get_header_image() ) : ?> <!-- If statement to check if header image is set-->
         <figure class="header-image-container transparent">
         <?php the_header_image_tag(); ?>    <header id="masthead" class="site-header">
                 <div class="site-branding">
@@ -61,7 +61,45 @@
                 </nav><!-- #site-navigation -->
             </header><!-- #masthead -->
         </figure>
+<!--    --><?php //endif; ?>
+
+    <?php else : ?> <!-- If statement to check if header image is set-->
+            <?php the_header_image_tag(); ?>    <header id="masthead" class="site-header-basic">
+                <div class="site-branding">
+                    <?php
+                    the_custom_logo(); ?>
+                    <div class="branding-text-container">
+                        <?php
+                        if ( is_front_page() && is_home() ) :
+                            ?>
+                            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                        <?php
+                        else :
+                            ?>
+                            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                        <?php
+                        endif; ?>
+                        <?php
+                        $coffeecan_description = get_bloginfo( 'description', 'display' );
+                        if ( $coffeecan_description || is_customize_preview() ) :
+                            ?>
+                            <p class="site-description"><?php echo $coffeecan_description; /* WPCS: xss ok. */ ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div><!-- .site-branding -->
+
+                <nav id="site-navigation" class="main-navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'coffeecan' ); ?></button>
+                    <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                    ) );
+                    ?>
+                </nav><!-- #site-navigation -->
+            </header><!-- #masthead -->
     <?php endif; ?>
+
 <!--    uncomment above to display the custom header element-->
 
 
