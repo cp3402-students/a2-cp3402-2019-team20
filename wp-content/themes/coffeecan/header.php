d110f26a85fb5c6a7f9f51c8b955f1e8d2022979
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-05-10 16:59:57 +1000

    site picture and top level menu is dynamic and changes to screen size appropriately

--- wp-content/themes/coffeecan/header.php
+++ wp-content/themes/coffeecan/header.php
@@ -13,94 +13,104 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'coffeecan' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'coffeecan'); ?></a>
 
-    <?php if ( get_header_image() ) : ?> <!-- If statement to check if header image is set-->
+    <?php if (get_header_image()) : ?> <!-- If statement to check if header image is set-->
         <figure class="header-image-container transparent">
-        <?php the_header_image_tag(); ?>    <header id="masthead" class="site-header">
+            <?php the_header_image_tag(); ?>
+            <header id="masthead" class="site-header">
                 <div class="site-branding">
                     <?php
                     the_custom_logo(); ?>
                     <div class="branding-text-container">
                         <?php
-                        if ( is_front_page() && is_home() ) :
+                        if (is_front_page() && is_home()) :
                             ?>
-                            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                      rel="home"><?php bloginfo('name'); ?></a></h1>
                         <?php
                         else :
                             ?>
-                            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                            <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                     rel="home"><?php bloginfo('name'); ?></a></p>
                         <?php
                         endif; ?>
                         <?php
-                        $coffeecan_description = get_bloginfo( 'description', 'display' );
-                        if ( $coffeecan_description || is_customize_preview() ) :
+                        $coffeecan_description = get_bloginfo('description', 'display');
+                        if ($coffeecan_description || is_customize_preview()) :
                             ?>
                             <p class="site-description"><?php echo $coffeecan_description; /* WPCS: xss ok. */ ?></p>
                         <?php endif; ?>
                     </div>
                 </div><!-- .site-branding -->
-
-                <nav id="site-navigation" class="main-navigation">
-                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'coffeecan' ); ?></button>
-                    <?php
-                    wp_nav_menu( array(
-                        'theme_location' => 'menu-1',
-                        'menu_id'        => 'primary-menu',
-                    ) );
-                    ?>
-                </nav><!-- #site-navigation -->
+                <div class="main-navigation-div">
+                    <nav id="site-navigation" class="main-navigation">
+                        <button class="menu-toggle" aria-controls="primary-menu"
+                                aria-expanded="false"><?php esc_html_e('Primary Menu', 'coffeecan'); ?></button>
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'menu-1',
+                            'menu_id' => 'primary-menu',
+                        ));
+                        ?>
+                    </nav>
+                </div><!-- #site-navigation -->
             </header><!-- #masthead -->
         </figure>
-<!--    --><?php //endif; ?>
+        <!--    --><?php //endif; ?>
 
     <?php else : ?> <!-- If statement to check if header image is set-->
-            <?php the_header_image_tag(); ?>    <header id="masthead" class="site-header-basic">
-                <div class="site-branding">
+        <?php the_header_image_tag(); ?>
+        <header id="masthead" class="site-header-basic">
+            <div class="site-branding">
+                <?php
+                the_custom_logo(); ?>
+                <div class="branding-text-container">
                     <?php
-                    the_custom_logo(); ?>
-                    <div class="branding-text-container">
-                        <?php
-                        if ( is_front_page() && is_home() ) :
-                            ?>
-                            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                        <?php
-                        else :
-                            ?>
-                            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-                        <?php
-                        endif; ?>
-                        <?php
-                        $coffeecan_description = get_bloginfo( 'description', 'display' );
-                        if ( $coffeecan_description || is_customize_preview() ) :
-                            ?>
-                            <p class="site-description"><?php echo $coffeecan_description; /* WPCS: xss ok. */ ?></p>
-                        <?php endif; ?>
-                    </div>
-                </div><!-- .site-branding -->
-
+                    if (is_front_page() && is_home()) :
+                        ?>
+                        <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                  rel="home"><?php bloginfo('name'); ?></a></h1>
+                    <?php
+                    else :
+                        ?>
+                        <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                 rel="home"><?php bloginfo('name'); ?></a></p>
+                    <?php
+                    endif; ?>
+                    <?php
+                    $coffeecan_description = get_bloginfo('description', 'display');
+                    if ($coffeecan_description || is_customize_preview()) :
+                        ?>
+                        <p class="site-description"><?php echo $coffeecan_description; /* WPCS: xss ok. */ ?></p>
+                    <?php endif; ?>
+                </div>
+            </div><!-- .site-branding -->
+            <div class="main-navigation-div">
                 <nav id="site-navigation" class="main-navigation">
-                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'coffeecan' ); ?></button>
+                    <button class="menu-toggle" aria-controls="primary-menu"
+                            aria-expanded="false"><?php esc_html_e('Primary Menu', 'coffeecan'); ?></button>
                     <?php
-                    wp_nav_menu( array(
+                    wp_nav_menu(array(
                         'theme_location' => 'menu-1',
-                        'menu_id'        => 'primary-menu',
-                    ) );
+                        'menu_id' => 'primary-menu',
+                    ));
                     ?>
-                </nav><!-- #site-navigation -->
-            </header><!-- #masthead -->
+                </nav>
+            </div><!-- #site-navigation -->
+        </header><!-- #masthead -->
     <?php endif; ?>
 
-<!--    uncomment above to display the custom header element-->
+    <!--    uncomment above to display the custom header element-->
 
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
