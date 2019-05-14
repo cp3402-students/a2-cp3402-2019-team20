1f156e23dcf7d854a13e0cdde7d3722a30efabf4
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-05-14 12:58:07 +1000

    add tables styling in _tables.scss add whitespace for tag links in template-tags.php add styles for separating each meta data post element with the "|" char and added margin, this is done with psuedo elements to add the pipe char after the post author and post date and before the comments and edit link fix comment spelling mistake in functions.php add Nunito font 400i to site google font request to add itialics option for the Nunito font if required

--- wp-content/themes/coffeecan/inc/template-tags.php
+++ wp-content/themes/coffeecan/inc/template-tags.php
@@ -69,7 +69,7 @@ if ( ! function_exists( 'coffeecan_entry_footer' ) ) :
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'coffeecan' ) );
 			if ( $tags_list ) {
 				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'coffeecan' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+				printf( ' <span class="tags-links">' . esc_html__( 'Tagged %1$s', 'coffeecan' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
 	}
