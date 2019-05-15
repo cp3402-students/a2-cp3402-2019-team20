2bc51a7bdb2077991a366a41834875f94d08aad9
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-05-15 22:42:21 +1000

    merging to master
    merge to master

--- wp-content/themes/coffeecan/js/scroll.js
+++ wp-content/themes/coffeecan/js/scroll.js
@@ -4,28 +4,37 @@ window.onscroll = function() {myFunction()};
 // Get the navbar
 // Get the header image
 var navbar = document.getElementById("masthead");
+<<<<<<< HEAD
 var img = document.getElementById("header-image");
+=======
 var img = document.getElementById("header-image-con");
+>>>>>>> themeDevMatt
 var imgHeight = img.clientHeight;
 var navHeight = navbar.clientHeight;
 
 // Get the offset position of the navbar
+<<<<<<< HEAD
 
 var sticky = navbar.offsetTop;
 var color = navbar.backgroundColor;
+=======
 var sticky = navbar.offsetTop;
 var color = navbar.backgroundColor
+>>>>>>> themeDevMatt
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset >= (imgHeight - navHeight)) {
+<<<<<<< HEAD
         navbar.style.backgroundColor="rgba(1,1,1,.9)";
         console.log(height)
     } else if (window.pageYOffset < (imgHeight - navHeight)) {
         navbar.style.backgroundColor="rgba(1,1,1,.3";
+=======
         transitionToDark()
     } else if (window.pageYOffset < (imgHeight - navHeight)) {
         transitionToLight()
+>>>>>>> themeDevMatt
     }
 }
 function transitionToDark() {
