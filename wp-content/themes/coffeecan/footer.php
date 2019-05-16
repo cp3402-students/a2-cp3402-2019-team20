3097d28a97959dc29986eac6978919e3b2b6a0b4
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-05-16 17:24:23 +1000

    fixed stylesheet error (missing curly brace) in _global.scss add import statement for the _footer.scss file in _site.scss re add $color__theme-comment-grey: #666; to _colors.scss changed footer text content in footer.php and added containers to have a left and right box for the footer.

--- wp-content/themes/coffeecan/footer.php
+++ wp-content/themes/coffeecan/footer.php
@@ -15,17 +15,12 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'coffeecan' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'coffeecan' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'coffeecan' ), 'coffeecan', '<a href="http://underscores.me/">Team-20</a>' );
-				?>
+                <div class="footer-text-container">
+                    <p class="footer-text">The Coffee Can, 28 Michaelangelo Drive, Redlynch, Cairns <?php echo date('Y')?> </p>
+                </div>
+                <div class="footer-links-container">
+                    <a href="https://www.facebook.com/Mycoffeecan"><p>Like us on Facebook</p></a>
+                </div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
