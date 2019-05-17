06e251cd5c5f2f9de98dd916b9cb412e70f50298
Matthew <matt.lewandowski@my.jcu.edu.au>
2019-05-17 19:08:36 +1000

    turned footer into 3 divs stacked vertically

--- wp-content/themes/coffeecan/footer.php
+++ wp-content/themes/coffeecan/footer.php
@@ -11,18 +11,22 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-                <div class="footer-text-container">
-                    <p class="footer-text">The Coffee Can, 28 Michaelangelo Drive, Redlynch, Cairns <?php echo date('Y')?> </p>
-                </div>
-                <div class="footer-links-container">
-                    <a href="https://www.facebook.com/Mycoffeecan"><p>Like us on Facebook</p></a>
-                </div>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+    <div class="site-info">
+        <div class="social-media-container">
+            <a href="https://www.facebook.com/Mycoffeecan"><p>Like us on Facebook</p></a>
+        </div>
+        <div class="footer-text-container">
+            <p class="slogan">The Coffee Can</p><br>
+            <p class="slogan-small">passionate about coffee</p>
+        </div>
+        <div class="footer-text-container">
+            <p class="footer-text">The Coffee Can, 28 Michaelangelo Drive, Redlynch, Cairns <?php echo date('Y') ?> </p>
+        </div>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
