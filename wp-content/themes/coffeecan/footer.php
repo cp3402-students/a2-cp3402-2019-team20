b81c1ac82b1fa03c67da3ddfdd339df579856273
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-05-22 22:10:36 +1000

    made footer social media links open a new tab when clicked

--- wp-content/themes/coffeecan/footer.php
+++ wp-content/themes/coffeecan/footer.php
@@ -19,11 +19,11 @@ $athing = get_template_directory();
     <div class="site-info">
         <div class="social-media-container">
             <p class="footer-text">Follow us</p>
-            <a href="https://www.facebook.com/Mycoffeecan">
+            <a href="https://www.facebook.com/Mycoffeecan" target="_blank">
                 <img src="/wp-content/themes/coffeecan/images/facebook.png">
 <!--                <img src="--><?php //echo get_template_directory() . '/images/facebook.png' ?><!--">-->
             </a>
-            <a href="https://www.instagram.com/the_coffee_can">
+            <a href="https://www.instagram.com/the_coffee_can" target="_blank">
                 <img src="/wp-content/themes/coffeecan/images/instagram.png">
 <!--                <img src="--><?php //echo get_template_directory() . '/images/instagram.png' ?><!--">-->
             </a>
